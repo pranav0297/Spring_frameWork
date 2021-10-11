0efc470bbb9627eab050b28356c142e810213a8c
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-10-11 15:19:55 +0530

    test

--- q5/test2.java
+++ q5/test2.java
@@ -1,2 +1,40 @@
-public class test2 {
+package com.spring.example1.spring_demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+interface Animal {
+   public void character();
 }
+@Component
+class Dog implements Animal {
+    Animal animal;
+    public void character() {
+        System.out.println("Bark");
+    }
+}
+@Component
+@Primary
+    class Cat implements Animal {
+        public void character() {
+            System.out.println("Meow");
+        }
+    }
+    
+    class test {
+        public static void main(String[] args) {
+            ApplicationContext context = new AnnotationConfigApplicationContext(testing.class);
+
+            Animal animal= context.getBean(Animal.class);
+            animal.character();
+
+        }
+    }
+
+
+
+
+
+
