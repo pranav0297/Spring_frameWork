d06ee97b3351b468bdb09d4f072ea3371c989f10
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-10-11 18:10:58 +0530

    test

--- q4/test1.java
+++ q4/test1.java
@@ -1,2 +1,20 @@
-public class test1 {
+package com.spring.example1.spring_demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+class application_context{
+     void test(){
+         System.out.println("Bean created");
+     }
+
+}
+class test{
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext( application_context.class);
+        application_context ac = context.getBean( application_context.class);
+        ac.test();
+    }
 }
