9a72d8c258e186266c25485c2602bdf0c23e2394
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-10-11 17:57:15 +0530

    test

--- q6/test3.java
+++ q6/test3.java
@@ -1,2 +1,18 @@
-public class test3 {
+package com.spring.example1.spring_demo;
+import org.springframework.stereotype.Component;
+
+@Component
+class Contruction_injection {
+    public void test1(){
+        System.out.println("test");
+    }
+}
+@Component
+class Contruction_injection2 {
+        private Contruction_injection contruction_injection;
+
+    public Contruction_injection2(Contruction_injection Contruction_injection) {
+        this.contruction_injection=contruction_injection;
+
+    }
 }
